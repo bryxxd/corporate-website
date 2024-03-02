18eed92af942a1c042ae76c478e3ff465ead73cf
brixdesalit7 <brixdesalit7>
2024-03-02 18:28:29 +0800

    added header

--- wp-content/themes/Template/header.php
+++ wp-content/themes/Template/header.php
@@ -4,17 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
-        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <?php wp_head() ?>
 </head>
 
 <header class="header">
     <div class="header__info">
-        <?php if (is_front_page()) : ?>
-        <h1 class="header__info__title">Logo</h1>
-        <?php else :  ?>
-        <p class="header__info__title">Logo</p>
+        <?php if (is_front_page()): ?>
+            <h1 class="header__info__title">Logo</h1>
+        <?php else: ?>
+            <p class="header__info__title">Logo</p>
         <?php endif ?>
     </div>
     <nav class="nav">
@@ -34,6 +35,9 @@
             </li>
             <li class="nav__ul__li">
                 <a href="<?php echo home_url('/about/') ?>" class="nav__ul__li__a">About</a>
+            </li>
+            <li class="nav__ul__li">
+                <a href="<?php echo home_url('/contact/') ?>" class="nav__ul__li__a is-contact">Contact</a>
             </li>
         </ul>
     </nav>
