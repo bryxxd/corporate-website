eabf680c715a29d49927f05df0a57328ff7ca705
Bryxx <89675158+brixdesalit7@users.noreply.github.com>
2026-01-24 16:47:05 +0800

    combine enqueue functions

--- wp-content/themes/Template/include/include.php
+++ wp-content/themes/Template/include/include.php
@@ -1,7 +1,10 @@
 <?php
-// Include main stylesheet
-wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
-
-
-// Include main script 
-wp_enqueue_script('script', get_template_directory_uri() . '/assets/js/base/index.js', array('jquery'), 1.1, true);
+function add_theme_scripts()
+{
+    // Include main stylesheet
+    wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
+    
+    // Include main script 
+    wp_enqueue_script('script', get_template_directory_uri() . '/assets/js/base/index.js', array('jquery'), 1.1, true);
+}
+add_action('wp_enqueue_scripts', 'add_theme_scripts');
